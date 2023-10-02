ec5c223743a6a0bc74bb4d186e13dcef6314722c
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-10-02 16:26:28 +0200

    a

--- Tema 1/Tema 1/Ejercicios tema 1/Ejercicios/bin/main.dart
+++ Tema 1/Tema 1/Ejercicios tema 1/Ejercicios/bin/main.dart
@@ -8,7 +8,7 @@ void main(List<String> arguments) {
   stdout.writeln(ecuaciones.numSolucionesEcGrado2(1, 0, 0)); // 1
   stdout.writeln(ecuaciones.numSolucionesEcGrado2(2, 3, 4)); // 0
   */
-
+  
   // Ejercicio 3
   /*
   stdout.writeln(ecuaciones.coeficiente(1)); // "1"
@@ -20,9 +20,8 @@ void main(List<String> arguments) {
   */
 
   // Ejercicio 4
-  
-  stdout.writeln(
-      ecuaciones.polinomioGrado2Str(a: 1, b: -5, c: -2)); // "x2 -5x –2"
+  /*
+  stdout.writeln(ecuaciones.polinomioGrado2Str(a: 1, b: -5, c: -2)); // "x2 -5x –2"
   stdout.writeln(ecuaciones.polinomioGrado2Str(a: -2, c: -2)); // "–2x2 –2"
   stdout.writeln(ecuaciones.polinomioGrado2Str(a: -2, b: 3)); // "–2x2 +3x"
   stdout.writeln(ecuaciones.polinomioGrado2Str(b: 1, c: -2)); // "x –2"
@@ -30,14 +29,14 @@ void main(List<String> arguments) {
   stdout.writeln(ecuaciones.polinomioGrado2Str(b: 8)); // "8x"
   stdout.writeln(ecuaciones.polinomioGrado2Str(c: 5)); // "5"
   stdout.writeln(ecuaciones.polinomioGrado2Str()); // "0"
-  
+  */
 
   // Ejercicio 5
-  /*
+  
   String jugar;
   do {
     ecuaciones.solucionadorEcuacionesGrado2();
     stdout.writeln("¿Deseas resolver otra ecuación? (s/n)");
     jugar = stdin.readLineSync()?.toUpperCase() ?? 'N';
-  } while (jugar == 'S'); */
+  } while (jugar == 'S'); 
 }
