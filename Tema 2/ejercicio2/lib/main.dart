0494aa90e9cd39068b0df75ac8c924b8ef9c04b0
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-11-20 17:38:54 +0100

    a

---
+++ Tema 2/ejercicio2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:clase1/ejercicio3.dart';
+import 'package:clase1/ejercicio4.dart';
+import 'package:clase1/ejercicio5.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ejercicios Flutter',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const Ejercicio5(),
+    );
+  }
+}
